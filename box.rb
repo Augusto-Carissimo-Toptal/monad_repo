80c36693a4cd53e4af2153e64c4f25defef3b44d
Marta <marta.karas@toptal.com>
2023-03-09 18:37:06 +0100

    Add rspec

--- box.rb
+++ box.rb
@@ -1,3 +1,11 @@
+require "bundler/inline"
+
+gemfile do
+  gem "rspec"
+end
+
+require "rspec/autorun"
+
 class Box
   attr_reader :value
   def initialize(value)
@@ -16,5 +24,13 @@ class Box
     else
       raise 'Is not a Box'
     end
+  end
+end
+
+
+RSpec.describe "inline Bundler and autorun RSpec" do
+  it "" do
+    meth = ->(value) {Box.return(value + 1)} 
+    expect(meth.call(1)).to eq(Box.return(1).bind(&meth))
   end
 end
