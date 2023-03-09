c10f406028f7f4b864aa93d476a42f5fd5de448c
vaporyhumo <roanvilina@gmail.com>
2023-03-09 14:45:51 -0300

    Refactor

--- box.rb
+++ box.rb
@@ -8,29 +8,42 @@ require "rspec/autorun"
 
 class Box
   attr_reader :value
+  
   def initialize(value)
     @value = value
   end
 
   private_class_method(:new)
+
   def self.return(arg)
     new(arg)
   end
 
   def bind(&block)
-    result = block.call(value)
-    if result.is_a?(Box)
-      result
-    else
-      raise 'Is not a Box'
+    block.call(value).tap do |new_box|
+      raise 'return value of Box#bind is not a Box' unless new_box.is_a?(Box)
     end
+  end
+
+  def map(&blk)
+    Box.return(blk.call(value))
+  end
+
+  def ==(other)
+    raise unless other.is_a?(Box)
+
+    other.value == value
   end
 end
 
 
 RSpec.describe "inline Bundler and autorun RSpec" do
   it "" do
-    meth = ->(value) {Box.return(value + 1)} 
+    meth = ->(value) do 
+      Box.return(value + 1)
+    end
+
     expect(meth.call(1)).to eq(Box.return(1).bind(&meth))
   end
 end
+
